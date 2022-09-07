b192077bb5acf06803c0f3c59c24e823284bf295
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-07 11:54:15 -0400

    add tap gesture (two touch) to resize image

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     var body: some View {
         NavigationView{
             ZStack{
@@ -10,9 +13,29 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+
+                //MARK: TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()){
+                                imageScale = 1
+                            }
+                        }
+                    })
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            })
         }.navigationViewStyle(.stack)
     }
 }
