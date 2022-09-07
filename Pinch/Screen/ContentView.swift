29d46436d472ae60301c91c08f6e2ec69e89d3b4
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-07 18:01:54 -0400

    add magnification gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -50,6 +50,29 @@ struct ContentView: View {
                                 resetImageState()
                             }
                         })
+                
+                //MARK: MAGNIFICATION
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)){
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                        
+                                        if imageScale > 5 {
+                                            imageScale = 5
+                                        }
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
